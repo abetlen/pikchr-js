d7c22949a06eda50f1c3596e5e0d21044a7e0f44
abetlen <abetlen@gmail.com>
2026-06-03 01:50:31 -0700

    fix: restore Pikchr result and flag type names

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,19 @@
 declare namespace loadPikchr {
-  type Result = {
+  type PikchrResult = {
     svg: string;
     width: number;
     height: number;
   };
 
-  type Flags = {
+  type PikchrFlags = {
     PLAINTEXT_ERRORS: 0x0001;
     DARK_MODE: 0x0002;
   };
 
   type Pikchr = {
     (pikchrString: string, className?: string, flags?: number, height?: number, width?: number): string;
-    render(pikchrString: string, className?: string, flags?: number): Result;
-    flags: Flags;
+    render(pikchrString: string, className?: string, flags?: number): PikchrResult;
+    flags: PikchrFlags;
   };
 }
 
