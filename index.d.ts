e4166d558bc97afaba36ba7a49c71e9ee98e4b0c
abetlen <abetlen@gmail.com>
2026-06-03 01:41:38 -0700

    fix: export TypeScript declarations

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,22 @@
-type PikchrResult = {
-  svg: string;
-  width: number;
-  height: number;
-};
-type PikchrFlags = {
-  PLAINTEXT_ERRORS: 0x0001;
-  DARK_MODE: 0x0002;
-};
-type Pikchr = {
-  (pikchrString: string, className?: string, flags?: number, height?: number, width?: number): string;
-  render(pikchrString: string, className?: string, flags?: number): PikchrResult;
-  flags: PikchrFlags;
-};
-declare function loadPikchr(): Promise<Pikchr>
+declare namespace loadPikchr {
+  type Result = {
+    svg: string;
+    width: number;
+    height: number;
+  };
+
+  type Flags = {
+    PLAINTEXT_ERRORS: 0x0001;
+    DARK_MODE: 0x0002;
+  };
+
+  type Renderer = {
+    (pikchrString: string, className?: string, flags?: number, height?: number, width?: number): string;
+    render(pikchrString: string, className?: string, flags?: number): Result;
+    flags: Flags;
+  };
+}
+
+declare function loadPikchr(): Promise<loadPikchr.Renderer>;
+
+export = loadPikchr;
